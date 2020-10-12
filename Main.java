2ff9fab842187ca215fbd0dbc079518803f21181
alexnievesp7 <alejandro.nieves@patelhs.org>
2020-10-12 16:40:30 +0000

    new changes

--- Main.java
+++ Main.java
@@ -1,5 +1,15 @@
-class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
+import java.util.Scanner;
+public class CovidSurvey {
+  public static void main(String{} args) {
+    System.out.println("Welcome back to school");
+    System.out.println("");
+    Scanner scan = new Scanner(System.in);
+    System.out.println("What is your name?");
+    String name = scan.next();
+    System.out.println("How are you feeling?");
+    String mood = scan.next();
+    System.out.println("Have you been tested for Covid recently?");
+    String test = scan.next();
+    
   }
 }
